a57a4ff98f7063d6f2ea5fad0ff4ec26885e4b93
Test <Test@DESKTOP-0S754EC.TECHMOJO.LOCAL>
2022-07-25 14:14:03 +0530

    prjct

--- shoppingcart/src/Components/Header/Header.js
+++ shoppingcart/src/Components/Header/Header.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import './Header.css'
 
+
+
 export default function Header() {
+
+
+
   return (
     <div className='head'>
-      <div>
+      <div className='heading-container'>
           <h1 className='heading'>Shopping cart</h1>
       </div>
       
       <div>
         <div>
-        <input type="text" placeholder='Search product'/>
+        <input type="text" placeholder='Search by name'/>
         </div>
         
         <div>Sort</div>
+        <div>
+          
+        </div>
+        
       </div>
     </div>
   )
